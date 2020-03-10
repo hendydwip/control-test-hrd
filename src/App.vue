705a15c1681f91bcd64ba4b3c9d94556e47eaac7
Hendy <hendy.putranto@kompas.com>
2020-03-10 14:56:53 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class='w-full'>
+    <div class='bg-gray-300 flex p-3 justify-center w-full'>
+      <header-bar></header-bar>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderBar from './components/bar/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderBar
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/bar/Header.vue
@@ -0,0 +1,56 @@
+<template>
+<div class='flex flex-row items-center'>
+  <div class='uppercase font-bold'>
+    HRD Kayaknya
+  </div>
+  <ul class="flex">
+    <router-link to='/' tag='li' class='menu-inactive' active-class='menu-active' exact><a> Dashboard </a></router-link>
+    <router-link to='/employees' tag='li' class='menu-inactive' active-class='menu-active'><a> Employees </a></router-link>
+    <router-link to='/applicants' tag='li' class='menu-inactive' active-class='menu-active'><a> Applicants </a></router-link>
+    <router-link to='/attedance' tag='li' class='menu-inactive' active-class='menu-active'><a> Attedance </a></router-link>
+    <router-link to='/leave' tag='li' class='menu-inactive' active-class='menu-active'><a> Leave Request </a></router-link>
+  </ul>
+  <div>
+    Tempat nama User
+  </div>
+  <div class='menu-inactive cursor-pointer'>
+    Logout
+  </div>
+</div>  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.menu-inactive{
+  @apply mx-2;
+  @apply inline-block;
+  @apply border;
+  @apply border-white;
+  @apply rounded;
+  @apply text-blue-500;
+  @apply py-1;
+  @apply px-3;
+}
+
+.menu-inactive:hover{
+  @apply border-gray-200;
+  @apply bg-gray-200;
+
+}
+
+.menu-active{
+  @apply inline-block;
+  @apply border;
+  @apply border-blue-500;
+  @apply rounded;
+  @apply py-1;
+  @apply px-3;
+  @apply bg-blue-500;
+  @apply text-white;
+}
+</style>
