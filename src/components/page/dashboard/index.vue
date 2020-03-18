f6bf335f14892139ad4de57307eb62d9d2c177e1
Wahyu Kharisma <made.wahyu@kompas.com>
2020-03-18 14:07:34 +0700

    Adding style dashboard

--- src/components/page/dashboard/index.vue
+++ src/components/page/dashboard/index.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="bg-gray-100 flex h-screen items-center justify-center w-full">
-    <div class="w-full max-w-xs">
-      <p class="text-center text-sm text-gray-500">Total Employees</p>
-      <p class="text-center text-sm text-gray-500">{{getEmployee()}}</p>
-      <p class="text-center text-sm text-gray-500">{{getGenderEmployee(1)}} Males/{{getGenderEmployee(2)}} Females</p>
+    <div class=" w-full max-w-xs shadow bg-gray-50 rounded m-5">
+        <div class="p-4 bg-white rounded-t border-b text-center">Total Employees</div>
+        <p class="text-center text-5xl text-gray-500">{{getEmployee()}}</p>
+        <p class="p-4 text-center text-sm text-gray-500">{{getGenderEmployee(1)}} Males/{{getGenderEmployee(2)}} Females</p>
     </div>
-    <div class="w-full max-w-xs">
-      <p class="text-center text-sm text-gray-500">Out Today</p>
-      <p class="text-center text-sm text-gray-500">.. People</p>
+    
+    <div class=" w-full max-w-xs shadow bg-gray-50 rounded m-5">
+        <div class="p-4 bg-white rounded-t border-b text-center">Out Today</div>
+        <p class="text-center text-5xl text-gray-500">-</p>
+        <p class="p-4 text-center text-sm text-gray-500">People</p>
     </div>
-    <div class="w-full max-w-xs">
-      <p class="text-center text-sm text-gray-500">Today's Present</p>
-      <p class="text-center text-sm text-gray-500">0/{{getEmployee()}}</p>
+
+    <div class=" w-full max-w-xs shadow bg-gray-50 rounded m-5">
+        <div class="p-4 bg-white rounded-t border-b text-center">Today's Present</div>
+        <p class="text-center text-5xl text-gray-500">0/{{getEmployee()}}</p>
+        <p class="p-4 text-center text-sm text-gray-500">People</p>
     </div>
+
   </div>
 </template>
 
