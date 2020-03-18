f86aec7d27380f68791def01115fcbd1c7cef02a
Wahyu Kharisma <made.wahyu@kompas.com>
2020-03-18 14:41:22 +0700

    Adding new upcoming events and applicant

--- src/components/page/dashboard/index.vue
+++ src/components/page/dashboard/index.vue
@@ -1,23 +1,55 @@
 <template>
-  <div class="bg-gray-100 flex h-screen items-center justify-center w-full">
-    <div class=" w-full max-w-xs shadow bg-gray-50 rounded m-5">
-        <div class="p-4 bg-white rounded-t border-b text-center">Total Employees</div>
-        <p class="text-center text-5xl text-gray-500">{{getEmployee()}}</p>
-        <p class="p-4 text-center text-sm text-gray-500">{{getGenderEmployee(1)}} Males/{{getGenderEmployee(2)}} Females</p>
-    </div>
-    
-    <div class=" w-full max-w-xs shadow bg-gray-50 rounded m-5">
-        <div class="p-4 bg-white rounded-t border-b text-center">Out Today</div>
-        <p class="text-center text-5xl text-gray-500">-</p>
-        <p class="p-4 text-center text-sm text-gray-500">People</p>
+  <div class="bg-gray-100 items-center justify-center w-full">
+    <div class="bg-gray-100 flex items-center justify-center w-full">
+        <div class="w-full max-w-xs shadow bg-gray-50 rounded m-5">
+            <div class="p-4 bg-white rounded-t border-b text-center">Total Employees</div>
+            <p class="text-center text-5xl text-gray-500">{{getEmployee()}}</p>
+            <p class="p-4 text-center text-sm text-gray-500">{{getGenderEmployee(1)}} Males/{{getGenderEmployee(2)}} Females</p>
+        </div>
+        
+        <div class=" w-full max-w-xs shadow bg-gray-50 rounded m-5">
+            <div class="p-4 bg-white rounded-t border-b text-center">Out Today</div>
+            <p class="text-center text-5xl text-gray-500">-</p>
+            <p class="p-4 text-center text-sm text-gray-500">People</p>
+        </div>
+
+        <div class=" w-full max-w-xs shadow bg-gray-50 rounded m-5">
+            <div class="p-4 bg-white rounded-t border-b text-center">Today's Present</div>
+            <p class="text-center text-5xl text-gray-500">0/{{getEmployee()}}</p>
+            <p class="p-4 text-center text-sm text-gray-500">People</p>
+        </div>
     </div>
 
-    <div class=" w-full max-w-xs shadow bg-gray-50 rounded m-5">
-        <div class="p-4 bg-white rounded-t border-b text-center">Today's Present</div>
-        <p class="text-center text-5xl text-gray-500">0/{{getEmployee()}}</p>
-        <p class="p-4 text-center text-sm text-gray-500">People</p>
+    <div class="bg-gray-100 flex items-center justify-center w-full">
+        <div class="flex flex-col md:flex-row">
+            <div class="md:w-full shadow bg-gray-100 rounded m-4">
+                <div class="p-4 bg-white rounded-t border-b text-center">Upcoming Events</div>
+                <div class="p-4">
+                    <table class="table-auto w-full bg-white border text-sm">
+                        <tr>
+                            <td class="px-4 py-2 border-b"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg></td>
+                            <td class="px-4 py-2 border-b">Test</td>
+                            <td class="px-4 py-2 border-b">Test</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col md:flex-row">
+            <div class="md:w-full shadow bg-gray-100 rounded m-4">
+                <div class="p-4 bg-white rounded-t border-b text-center">New Applicant</div>
+                <div class="p-4">
+                    <table class="table-auto w-full bg-white border text-sm">
+                        <tr>
+                            <td class="px-4 py-2 border-b"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></td>
+                            <td class="px-4 py-2 border-b">Test</td>
+                            <td class="px-4 py-2 border-b">Test</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
-
   </div>
 </template>
 
