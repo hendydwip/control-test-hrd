8b3dfe0e38641cad7853a937c8aba8abea04bd0c
Wahyu Kharisma <made.wahyu@kompas.com>
2020-03-18 15:47:34 +0700

    Adding function call events and applicant

--- src/components/page/dashboard/index.vue
+++ src/components/page/dashboard/index.vue
@@ -26,10 +26,10 @@
                 <div class="p-4 bg-white rounded-t border-b text-center">Upcoming Events</div>
                 <div class="p-4">
                     <table class="table-auto w-full bg-white border text-sm">
-                        <tr>
+                        <tr v-for='(index,key) in getEvents' :key='key'>
                             <td class="px-4 py-2 border-b"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg></td>
-                            <td class="px-4 py-2 border-b">Test</td>
-                            <td class="px-4 py-2 border-b">Test</td>
+                            <td class="px-4 py-2 border-b">{{index.date}}</td>
+                            <td class="px-4 py-2 border-b">{{index.description}}</td>
                         </tr>
                     </table>
                 </div>
@@ -40,10 +40,11 @@
                 <div class="p-4 bg-white rounded-t border-b text-center">New Applicant</div>
                 <div class="p-4">
                     <table class="table-auto w-full bg-white border text-sm">
-                        <tr>
+                        <tr v-for='(index,key) in getApplicants' :key='key'>
                             <td class="px-4 py-2 border-b"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></td>
-                            <td class="px-4 py-2 border-b">Test</td>
-                            <td class="px-4 py-2 border-b">Test</td>
+                            <td class="px-4 py-2 border-b">{{index.name}}</td>
+                            <td class="px-4 py-2 border-b">{{position(index.job_role)}}</td>
+                            <td class="px-4 py-2 border-b">{{index.schedule_pt}}</td>
                         </tr>
                     </table>
                 </div>
@@ -61,29 +62,41 @@ export default {
       const data = window.$cookies.get('user')
       return data
     },
+    getEvents(){
+        return this.event
+    },
+    getApplicants(){
+        return this.applicant
+    },
      ...mapGetters({
       employee: 'employee/employee',
+      event: 'event/event',
+      applicant: 'applicant/applicant',
     }),
   },
   data(){
     return{
-        test : 'data',
+       
     }
   },
   methods:{
-       testData(){
-           return this.test;
-       },
        getEmployee(){
            return this.employee.length
        },
+
        getGenderEmployee(gender){
         const tampung = this.employee.filter((jr) => {
           return jr.gender == gender
         })
         return tampung.length
-       }
-  }
+       },
+
+       position(id){
+            if(!this.job_role) return
+            const tampung = this.job_role.filter((jr) => {  return jr.id == id})
+            return tampung[0].position
+        }
+    }
 }
 </script>
 
