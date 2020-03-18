4a34aad77f17c4fe9cf9b8452a6fb2dea3333b0e
Hendy <hendydwip@hotmail.com>
2020-03-18 13:52:54 +0700

    'updateAttendanceStyle'

--- src/components/page/attendance/index.vue
+++ src/components/page/attendance/index.vue
@@ -1,30 +1,28 @@
 <template>
   <div class='w-full flex flex-col max-w-4xl mx-auto w-full'>
     <div class='flex flex-row justify-start w-full'>
-      <span>
-        {{userLogin}}
-      </span>
       <button class='p-1 m-1 text-center text-white rounded-lg bg-blue-400 cursor-pointer' @click='pushCheckIn("checkin")'>Check in</button>
       <button class='p-1 m-1 text-center text-white rounded-lg bg-red-400 cursor-pointer' @click='pushCheckIn("checkout")'>Check out</button>
     </div>
 
-    <button class='m-4 p-4 border rounded-lg' @click='typeShow == "checkin"?typeShow = "checkout":typeShow = "checkin"'>Change Show</button>
-
-    <div class='flex flex-col w-full'>
-      <div class='flex flex-row'>
-        <div class='flex mx-4 w-1/6'>Employee</div>
-        <div class='flex mx-4 w-1/6' v-for='(parent,key) in attendance.slice(attendance.length-5,attendance.length)' :key='key'>
-          <div class='flex flex-col'>
-            <div>{{parent.date}}</div>
-          </div>
+    <button class='m-4 p-4 bg-blue-400 text-white border rounded-lg w-1/6 mx-auto' @click='typeShow == "checkin"?typeShow = "checkout":typeShow = "checkin"'>Change Show</button>
+    
+    <div class='flex flex-col w-full capitalize my-4'>
+      Showing Status : {{typeShow}}
+    </div>
+    <div class='flex flex-col overflow-y-auto max-h-75 w-full'>
+      <div class='flex flex-row bg-gray-700 text-white'>
+        <div class='flex border w-1/6 py-4 justify-center'>Employee</div>
+        <div class='flex border w-1/6 py-4' v-for='(parent,key) in attendance.slice(attendance.length-5,attendance.length)' :key='key'>
+          <div class='text-center w-full'>{{parent.date}}</div>
         </div>
       </div>
 
       <div class='flex flex-col w-full'>
         <div class='flex w-full flex-row' v-for='(item,key) in employee' :key='key'>
-          <div class='flex mx-4 w-1/6'>{{item.name}}</div>
-          <div class='flex mx-4 w-1/6' v-for='(parentemp,key) in attendance.slice(attendance.length-5,attendance.length)' :key='key'>
-            <div class='flex flex-col'>
+          <div class='bg-gray-200 justify-center flex border text-blue-600 py-4 w-1/6'>{{item.name}}</div>
+          <div class='flex border w-1/6 py-4' v-for='(parentemp,key) in attendance.slice(attendance.length-5,attendance.length)' :key='key' :class='haveCheck(parentemp.data, item.id)?"bg-blue-500":"bg-red-500"'>
+            <div class='flex flex-col text-center text-white w-full'>
               <div>{{haveCheck(parentemp.data, item.id)?haveCheck(parentemp.data, item.id):"-"}}</div>
             </div>
           </div>
