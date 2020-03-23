0f555d0d18dd8930b8f42fa1e4023f85d1da7980
Wahyu Kharisma <made.wahyu@kompas.com>
2020-03-23 12:08:55 +0700

    Adding more style leave request

--- src/components/page/leave/index.vue
+++ src/components/page/leave/index.vue
@@ -3,36 +3,58 @@
     <div class='w-full mb-4'>
       <button @click='modal = true' class='p-4 text-white flex capitalize rounded-lg mx-4 bg-blue-400 cursor-pointer'>Add</button>
     </div>
-    <div class='flex justify-between mb-4 w-full'>
-      <button v-for='(item,key) in leave_status' :key='key' @click='filter == item.id? filter="" : filter=item.id' :class='filter == item.id?"bg-blue-600":""' class='p-4 text-white flex capitalize rounded-lg mx-4 bg-blue-400 cursor-pointer'>{{item.status}}</button>
-      <div class='capitalize my-4'>
-        Showing : {{filter?leave_status[filter-1].status:'All'}}
+    <div class="align-middle inline-block rounded min-w-full shadow overflow-hidden border-b border-gray-200">
+      <div class="border-b border-gray-300 text-center bg-white pt-4 shadow rounded-t">
+        <div class='flex justify-between mb-4 w-full'>
+          <button v-for='(item,key) in leave_status' :key='key' @click='filter == item.id? filter="" : filter=item.id' :class='filter == item.id?"bg-blue-600":""' class='p-4 text-white flex capitalize rounded-lg mx-4 bg-blue-400 cursor-pointer'>{{item.status}}</button>
+          <div class='capitalize my-4'>
+            Showing : {{filter?leave_status[filter-1].status:'All'}}
+          </div>
+        </div>
       </div>
+      <table class="min-w-full">
+        <tbody class="bg-white">
+          <tr class="hover:bg-gray-100" v-for='(leave,key) in filteredData' :key='key'>
+            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <div class="flex items-center">
+                <div class="flex-shrink-0 h-10 w-10">
+                  <a href="#" class="">
+                    <img src="https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="" class="h-10 w-10 rounded-full">
+                  </a>
+                </div>
+                <div class="ml-4">
+                  <div class="text-sm leading-5 font-medium text-gray-900">{{filterEmployeeLeave(leave.employee).name}}</div>
+                  <div class="text-sm leading-5 text-gray-500">{{filterEmployeeLeave(leave.employee).email}}</div>
+                  <div class="text-sm leading-5 text-gray-500">{{filterEmployeeLeave(leave.employee).phone}}</div>
+                </div>
+              </div>
+            </td>
+
+            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <div class="text-sm leading-5 text-gray-900">Leave Start - To</div>
+              <div class="text-sm leading-5 text-gray-500">{{leave.start_date}} - {{leave.end_date}}</div>
+            </td>
+
+             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <div class="text-sm leading-5 text-gray-900">Description</div>
+              <div class="text-sm leading-5 text-gray-500">{{leave.description}}</div>
+            </td>
+
+            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+              <div class="text-sm leading-5 text-gray-900">Status</div>
+              <div class="text-sm leading-5 text-gray-500">{{filterLeaveStatus(leave.status).status}}</div>
+            </td>
+
+            <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium" v-if='leave.status == 2'>
+              <button v-if='userLoginRole == 1' @click='updateStatusLeave(leave.id,1)' class='p-2 text-white flex capitalize rounded-lg bg-blue-400 cursor-pointer'>Approve</button>
+              <button v-if='userLoginRole == 1' @click='updateStatusLeave(leave.id,3)' class='p-2 text-white flex capitalize rounded-lg bg-red-400 cursor-pointer'>Rejected</button>
+              <button v-if='leave.employee == userLogin.id' @click='deteleRequest(leave.id)' class='p-2 text-white flex capitalize rounded-lg bg-red-700 cursor-pointer'>Delete</button> 
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
     
-    <div class='w-full border' v-for='(leave,key) in filteredData' :key='key'>
-      <div class='flex flex-row'>
-        <div class='w-1/6'>Name</div>
-        <div>{{filterEmployeeLeave(leave.employee).name}}</div>
-      </div>
-      <div class='flex flex-row'>
-        <div class='w-1/6'>Leave Start - To</div>
-        <div>{{leave.start_date}} - {{leave.end_date}}</div>
-      </div>
-      <div class='flex flex-row capitalize'>
-        <div class='w-1/6'>Status</div>
-        <div>{{filterLeaveStatus(leave.status).status}}</div>
-      </div>
-      <div class='flex flex-row'>
-        <div class='w-1/6'>Description</div>
-        <div>{{leave.description}}</div>
-      </div>
-      <div v-if='leave.status == 2' class='flex flex-row'>
-        <button v-if='userLoginRole == 1' @click='updateStatusLeave(leave.id,1)' class='p-2 text-white flex capitalize rounded-lg bg-blue-400 cursor-pointer'>Approve</button>
-        <button v-if='userLoginRole == 1' @click='updateStatusLeave(leave.id,3)' class='p-2 text-white flex capitalize rounded-lg bg-red-400 cursor-pointer'>Rejected</button>
-        <button v-if='leave.employee == userLogin.id' @click='deteleRequest(leave.id)' class='p-2 text-white flex capitalize rounded-lg bg-red-700 cursor-pointer'>Delete</button> 
-      </div>
-    </div>
 
 
 
