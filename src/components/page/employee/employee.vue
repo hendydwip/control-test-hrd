197543f875b57f30596ce0354ca79716fa4ecb46
Wahyu Kharisma <made.wahyu@kompas.com>
2020-03-23 11:30:12 +0700

    Adding more style employee form

--- src/components/page/employee/employee.vue
+++ src/components/page/employee/employee.vue
@@ -5,30 +5,45 @@
         Add Employee
       </router-link>
     </div>
-    <div class='w-full flex flex-row justify-between p-2 border-b'>
-      <div v-for='(index,key) in employee_status' :key='key' @click='filter == index.id? filter="" : filter=index.id' :class='filter == index.id?"bg-blue-400":""' class='p-4 rounded-lg mx-4 bg-blue-200 cursor-pointer'>
-        {{index.status}}
-      </div>  
+
+    <div class="align-middle inline-block rounded min-w-full shadow overflow-hidden border-b border-gray-200">
+      <div class="border-b border-gray-300 text-center bg-white pt-4 shadow rounded-t">
+        <div class='w-full flex flex-row justify-between p-2 border-b'>
+          <div v-for='(index,key) in employee_status' :key='key' @click='filter == index.id? filter="" : filter=index.id' :class='filter == index.id?"bg-blue-400":""' class='p-4 rounded-lg mx-4 bg-blue-200 cursor-pointer'>
+            {{index.status}}
+          </div>
+        </div>
+      </div>
+
+      <table class="min-w-full">
+        <tr class="hover:bg-gray-100" v-for='(index,key) in filterEmployee' :key='key'>
+          <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+            <div class="flex items-center">
+              <div class="flex-shrink-0 h-10 w-10">
+                <a href="#" class="">
+                  <img src="https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="" class="h-10 w-10 rounded-full">
+                  </a>
+              </div>
+              <div class="ml-4">
+                <div class="text-sm leading-5 font-medium text-gray-900">{{index.name}}</div>
+                <div class="text-sm leading-5 text-gray-500">{{index.email}}</div>
+                <div class="text-sm leading-5 text-gray-500">{{index.phone}}</div>
+              </div>
+            </div>
+          </td>
+          
+          <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+            <div class="text-sm leading-5 text-gray-900">{{division(index.job_role)}}</div>
+            <div class="text-sm leading-5 text-gray-500">{{position(index.job_role)}}</div>
+          </td>
+
+          <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+              <router-link :to='"/employees/edit/"+index.id' class='p-1 m-1 text-center rounded-lg bg-blue-200 cursor-pointer'>Edit User</router-link>
+              <div @click='modalSet(index.id)' v-if='roleUser[0].id === userLoginRole' class='p-1 m-1 text-center rounded-lg bg-blue-200 cursor-pointer'>Set User</div>
+          </td>
+        </tr>
+      </table>
     </div>
-    <div class='flex w-full border-b' v-for='(index,key) in filterEmployee' :key='key'> 
-      <div class='flex flex-col justify-start p-2 w-2/5'>
-        <div class='font-bold'>{{index.name}}</div>
-        <div>{{index.email}}</div>
-        <div>{{index.phone}}</div>
-      </div>
-      <div class='flex flex-col justify-start p-2 w-2/5'>
-        <div class='font-bold'>{{division(index.job_role)}}</div>
-        <div>{{position(index.job_role)}}</div>
-      </div>
-      <div class='flex flex-col justify-between p-1 w-1/5'>
-        <router-link :to='"/employees/edit/"+index.id' class='p-1 m-1 text-center rounded-lg bg-blue-200 cursor-pointer'>Edit</router-link>
-        <div @click='modalSet(index.id)' v-if='roleUser[0].id === userLoginRole' class='p-1 m-1 text-center rounded-lg bg-blue-200 cursor-pointer'>Set User</div>
-      </div>
-
-    </div>
-
-
-
 
     <Modal v-show="modal" @close='reset()'>
       <template v-slot:header>
